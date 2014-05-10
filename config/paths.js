dce53bf7264d159c3631672dd6c77815f0ac116e
Jason Rushton <github.com@schtuph.com>
2014-05-10 16:33:29 -0600

    convenience lib for handling global paths

---
+++ config/paths.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+	getPath: __getPath,
+
+	getBower: __getPath.bind(null, 'bower_components'),
+	getConfig: __getPath.bind(null, 'config'),
+	getData: __getPath.bind(null, 'appdata'),
+	getLib: __getPath.bind(null, 'lib'),
+	getModule: __getPath.bind(null, 'node_modules'),
+	getPublic: __getPath.bind(null, 'public'),
+	getRoute: __getPath.bind(null, 'routes'),
+	getView: __getPath.bind(null, 'views'),
+};
+
+function __getPath(pathRoot, partialPath) {
+	partialPath = partialPath || '';
+	return require('path').join(process.cwd(), pathRoot, partialPath);
+}
